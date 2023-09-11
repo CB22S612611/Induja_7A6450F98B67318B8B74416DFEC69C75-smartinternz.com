1eb2b2b2f164d597835fb170a98695c3c0f8ad5a
CB22S612611 <144400907+CB22S612611@users.noreply.github.com>
2023-09-11 18:22:59 +0530

    Add files via upload

---
+++ main (9).py
@@ -0,0 +1,10 @@
+def recursion_fact(x):
+  if x == 1:
+    return 1
+  else:
+    return (x * recursion_fact(x - 1))
+
+
+number = int(input("enter a value:"))
+if number >= 1:
+  print("the factorial of", number, "is", recursion_fact(number))
